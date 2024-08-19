8b6f2c1bd1b73bb43fb16fc928b1ae6a956d6d6a
JuanRodriguezUrbalejo <95185759+JuanRodriguezUrbalejo@users.noreply.github.com>
2024-08-18 22:48:17 -0700

    Codificacion de navbar
    se empezó con la codificación del navbar y se crearon los componentes: add-task, list, modify-task, task.
    Y se creó un service de prueba para el navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/modify-task/modify-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-task.component.html',
+  styleUrl: './modify-task.component.css'
+})
+export class ModifyTaskComponent {
+
+}
